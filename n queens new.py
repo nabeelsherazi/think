0b9176d2e9b804423f4b71b348f6a69f9d3e2235
nabeelsherazi <nabeelsherazi97@gmail.com>
2017-06-09 03:11:22 -0400

    reworked all this stuff

--- n queens new.py
+++ n queens new.py
@@ -1,3 +1,6 @@
+import math
+import random
+import time
 # Data structure planning
 # Board will be set up as a list of queen objects, which will be generated in
 # sequential order so that each next queen does not clash with any of the last
@@ -5,23 +8,36 @@
 #
 # Sample 8x8 board bd1 = [q1, q2, q3, q4, a5, q6, q7, q8] where qi = Queen(x, y)
 
+
 class Board:
-    """Creates board objects, which hold queens."""
+    """ Creates board objects, which hold queens. """
     def __init__(self, sz):
         self.queenslist = []
         self.size = sz
 
-    def add_queen(queen):
-        if len(self.queenslist) == self.sz:
+    def add_queen(self, queen):
+        if type(queen) is not Queen:
+            return "Pass queen objects only."
+        elif len(self.queenslist) == self.size:
             return "Board full."
         else:
             self.queenslist.append(queen)
 
 
 class Queen:
+    """ Creates queen objects, in a specified row and column. """
     def __init__(self, col, row):
         self.col = col
         self.row = row
+
+    def __eq__(self, other):
+        return self.col == other.col and self.row == other.row
+
+    def __ne__(self, other):
+        return not (self.col == other.col and self.row == other.row)
+
+    def __str__(self):
+        return "Queen in column {0}, row {1}".format(self.col, self.row)
 
     def share_diagonal(self, tgt):
         """Checks if tgt queen shares a diagonal with self."""
@@ -33,18 +49,48 @@ class Queen:
         for q in board.queenslist:
             if q.row == self.row:
                 return True
+            if self.share_diagonal(q):
+                return True
+        return False
 
 
-if __name__ == "__main__":
-    q1 = Queen(5, 2)
-    q2 = Queen(2, 0)
-    q3 = Queen(3, 0)
-    q4 = Queen(4, 3)
-    q5 = Queen(4, 1)
+def main():
+    rng = random.Random()
+    bd_sz = int(input("What is the board size? "))
+    bd = Board(bd_sz)
+    # Board is an object with attribute queenslist and method add_queens to add new queens
+    print("Warning: there are {0} possible cases for this board".format(math.factorial(bd_sz)))
+    t0 = time.clock()
+    for c in range(bd_sz):  # Need to place exactly bd_sz queens
+        tries = 0
+        queen_candidate = Queen(c, rng.randint(0, bd_sz - 1))  # Random seed
+        while queen_candidate.clashes_left(bd):
+            queen_candidate.row = rng.randint(0, bd_sz - 1)  # Alter candidate row until no longer clash
+            tries += 1
+        bd.add_queen(queen_candidate)  # Add copy of candidate to list
+        print("Solved {0} in {1} tries.".format(str(queen_candidate), tries))
+    t1 = time.clock()
+    dt = t1 - t0
+    time.sleep(1)
+    print("Solved board in {0:.1f} seconds.".format(dt))
+    print("Solution:")
+    for q in bd.queenslist:
+        print(q)
 
-    assert not q1.share_diagonal(q2)
-    assert q1.share_diagonal(q3)
-    assert q1.share_diagonal(q4)
-    assert q1.share_diagonal(q5)
-    print("all tests passed!")
-    input()
+
+main()
+input()
+
+# if __name__ == "__main__":
+#     q1 = Queen(5, 2)
+#     q2 = Queen(2, 0)
+#     q3 = Queen(3, 0)
+#     q4 = Queen(4, 3)
+#     q5 = Queen(4, 1)
+#
+#     assert not q1.share_diagonal(q2)
+#     assert q1.share_diagonal(q3)
+#     assert q1.share_diagonal(q4)
+#     assert q1.share_diagonal(q5)
+#     print("all tests passed!")
+#     input()
--- n queens.py
+++ n queens.py
@@ -1,8 +1,13 @@
+# New data structure: a list bd1 = [6, 4, 2, 0, 5, 7, 1, 3] represents the row
+# of the queens, since the col of each queen is equal to its index and cannot
+# be repeated. Note also that the row of any queen cannot be repeated, thus
+# for an N x N board, any solution must be a permutation of {1 ... N-1}
+# (since we are zero-indexed counting)
 
 def share_diagonal(x0, y0, x1, y1):
     """ Is (x0, y0) on a shared diagonal with (x1, y1)? """
     dy = abs(y1 - y0)        # Calc the absolute y distance
-    dx = abs(x1 - x0)        # CXalc the absolute x distance
+    dx = abs(x1 - x0)        # Calc the absolute x distance
     return dx == dy          # They clash if dx == dy
 
 def col_clashes(bs, c):
@@ -14,3 +19,39 @@ def col_clashes(bs, c):
               return True
 
     return False           # No clashes - col c has a safe placement.
+
+
+def has_clashes(the_board):
+    """ Determine whether we have any queens clashing on the diagonals.
+        We're assuming here that the_board is a permutation of column
+        numbers, so we're not explicitly checking row or column clashes.
+    """
+    for col in range(1, len(the_board)):
+        if col_clashes(the_board, col):
+            return True
+    return False
+
+
+def main():
+    import random
+    rng = random.Random()
+    bd = list(range(8))
+    num_found = 0
+    tries = 0
+    solns = []
+    for s in range(92):
+        while True:
+            rng.shuffle(bd)
+            tries += 1
+            if (not has_clashes(bd)) and (bd not in solns):
+                solns.append(bd[:])
+                print("found solution {0} in {1} tries.".format(bd, tries))
+                tries = 0
+                num_found += 1
+                break
+        print("total {0} unique solutions found".format(num_found))
+
+
+if __name__ == "__main__":
+    main()
+    input()
